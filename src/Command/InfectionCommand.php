60d9e4335d87b6d538d1b19f0f65144f5e661464
borNfreee <b0rn@list.ru>
2018-06-14 15:17:42 +0200

    Show time and memory info to the console output
    Signed-off-by: borNfreee <b0rn@list.ru>

--- src/Command/InfectionCommand.php
+++ src/Command/InfectionCommand.php
@@ -16,6 +16,8 @@ use Infection\Console\Exception\InfectionException;
 use Infection\Console\Exception\InvalidOptionException;
 use Infection\Console\LogVerbosity;
 use Infection\EventDispatcher\EventDispatcherInterface;
+use Infection\Events\ApplicationExecutionFinished;
+use Infection\Events\ApplicationExecutionStarted;
 use Infection\Finder\Exception\LocatorException;
 use Infection\Finder\Locator;
 use Infection\Mutant\Generator\MutationsGenerator;
@@ -190,6 +192,8 @@ final class InfectionCommand extends BaseCommand
         $metricsCalculator = $container->get('metrics');
         $container->get('subscriber.builder')->registerSubscribers($adapter, $output);
 
+        $this->eventDispatcher->dispatch(new ApplicationExecutionStarted());
+
         $processBuilder = new ProcessBuilder($adapter, $config->getProcessTimeout());
         $testFrameworkOptions = $this->getTestFrameworkExtraOptions($testFrameworkKey);
 
@@ -239,6 +243,8 @@ final class InfectionCommand extends BaseCommand
         );
         /** @var TestRunConstraintChecker $constraintChecker */
         $constraintChecker = $container->get('test.run.constraint.checker');
+
+        $this->eventDispatcher->dispatch(new ApplicationExecutionFinished());
 
         if (!$constraintChecker->hasTestRunPassedConstraints()) {
             $this->consoleOutput->logBadMsiErrorMessage(
